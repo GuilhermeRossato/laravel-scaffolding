ea90f56dd37f91a1cc4e92aaa9b029a713f19922
Guilherme Rossato <guilhermrossato@gmail.com>
2018-06-29 01:06:41 -0300

    Added getHttpHost method to Request class

--- lib/Request.php
+++ lib/Request.php
@@ -13,6 +13,27 @@ class Request {
         return $this->method;
     }
 
+    /**
+     * Gets the host with the port as string, port is ommited if it's 80
+     * 
+     * @return string The host and the port in the format "host:port"
+     */
+    public function getHttpHost() {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            return $_SERVER['HTTP_HOST'];
+        }
+        if (isset($_SERVER['SERVER_HOST'])) {
+            if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != "80") {
+                return $_SERVER['SERVER_HOST'].':'.$_SERVER['SERVER_PORT'];
+            }
+            return $_SERVER['SERVER_HOST'];
+        }
+        if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != "80") {
+            return 'localhost:'.$_SERVER['SERVER_PORT'];
+        }
+        return 'localhost';
+    }
+
     public function input($name) {
         if (isset($_POST[$name])) {
             return $_POST[$name];
